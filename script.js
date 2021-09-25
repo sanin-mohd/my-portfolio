db1890cf8cac660b010d5bb7e7842d73c937bc3b
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-09-25 19:22:22 +0530

    message sent ?

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    // $("#submit-form").validate({
+    //     rules:{
+    //         name:{
+    //             required:true,
+    //             minlength:4,
+    //             maxlength:20
+    //         },
+    //         email:{ 
+    //             required:true,
+    //             email:true
+    //         },
+    //         message:{
+    //             required:true
+    //         },
+    //         subject:{
+    //             required:true,
+    //             minlength:5
+    //         }
+    //     },
+        
+    // }),
+    $("#submit-form").submit((e) => {
+        var isvalidate = $("#submit-form")[0].checkValidity();
+        if (isvalidate) {
+        e.preventDefault()
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbwBaoNTlUKswLJH4uIwptkGktEzLt_de9I3UCINuYBQ7dkzJ1IDiWbEsbXc9-9I_2vN/exec",
+            data: $("#submit-form").serialize(),
+            method: "post",
+            success: function (response) {
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error: function (err) {
+                alert("Something Error")
+    
+            }
+        })
+    }
+    })
+    // $('#submit-form')[0].checkValidity();
+})
