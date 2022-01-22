ff7265fc7ba848d6c494c19035fa4ce13694c71f
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2022-01-22 20:45:43 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import Swal from 'sweetalert2'
+
+// // CommonJS
+ 
+
+
+
 $(document).ready(function(){
     $("#submit-form").validate({
         rules:{
@@ -21,15 +28,25 @@ $(document).ready(function(){
         
     }),
     $("#submit-form").submit((e) => {
+        e.preventDefault();
         var isvalidate = $("#submit-form")[0].checkValidity();
+       
         if (isvalidate) {
-        e.preventDefault()
+        
         $.ajax({
             url: "https://script.google.com/macros/s/AKfycbyGDcHoxunKLZiTdWztb0R0jSi1dC9ajr7rRpaNXFYjA5G0XUzAb3toMEV5e6JOr4qi/exec",
             data: $("#submit-form").serialize(),
             method: "post",
             success: function (response) {
                 alert("Form submitted successfully")
+                const Swal = require('sweetalert2')
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Your work has been saved',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
                 window.location.reload()
                 //window.location.href="https://google.com"
             },
